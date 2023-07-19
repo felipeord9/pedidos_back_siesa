6fbeb9fbcf695a8ed9c228f134c31e0a0e69461c
unknown <jhonatancastrillon34@gmail.com>
2023-07-19 11:48:50 -0500

    fix cors

--- src/index.js
+++ src/index.js
@@ -7,17 +7,9 @@ const { port, host } = config;
 
 const app = express();
 
-const whitelist = [
-  'http://pedidos.granlangostino.net:5515/'
-]
-const corsOptions = {
-  origin: function (origin, callback) {
-    if(whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
+var corsOptions = {
+  origin: 'http://pedidos.granlangostino.net:5515',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
 app.use(express.json());

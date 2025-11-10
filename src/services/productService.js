50f4c2561d8bbec6bf6aa75fc798c9479013eb2e
felipe <felipeord9@gmail.com>
2025-11-10 14:55:42 -0500

    amplia consulta de productos

--- src/services/productService.js
+++ src/services/productService.js
@@ -3,7 +3,7 @@ const { models } = require("../libs/sqlServer");
 
 const find = () => {
   const products = models.ExtensionItem.findAll({
-    limit: 2500,
+    limit: 3000,
     include: {
       association: "item",
       as: "item",
